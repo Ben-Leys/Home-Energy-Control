9a70a33d0dfb04f4188dffe6efb67ab6c0984cee
Ben Leys <benjaminleys@gmail.com>
2026-04-13 16:10:21 +0200

    Bug: house consumption not taking into account solar production charging into the car.

--- hec/logic_engine/scheduled_tasks.py
+++ hec/logic_engine/scheduled_tasks.py
@@ -528,7 +528,7 @@ def task_run_battery_predictor(app_config, db_handler: DatabaseHandler):
             evcc_w = GLOBAL_APP_STATE.get('evcc_loadpoint_state', {}).get('charge_current', 0) * 230
 
             gross_consumption_w = avg_1m_prod_w + avg_1m_grid_w - min(0, bat_w)
-            house_consumption_w = max(gross_consumption_w - evcc_w, 0)
+            house_consumption_w = gross_consumption_w - evcc_w
 
             opt_plan_df = bp.optimize_plan(base_plan_df, now_utc, actual_soc, GLOBAL_APP_STATE, app_config, db_handler,
                                            avg_1m_prod_w, house_consumption_w)
